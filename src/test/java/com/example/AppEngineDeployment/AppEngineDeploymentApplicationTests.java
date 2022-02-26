c2c1d509cdcd7e27b5ebf764a97f0f9eaa34d926
Priyanka <priyankabharae7apr@gmail.com>
2022-02-25 16:31:16 -0800

    test demo

---
+++ src/test/java/com/example/AppEngineDeployment/AppEngineDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppEngineDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEngineDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
